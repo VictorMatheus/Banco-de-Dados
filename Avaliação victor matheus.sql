12a1de31e26f41e0fbda10eb8e189be598542afd
VictorMatheus <105288329+VictorMatheus@users.noreply.github.com>
2023-05-19 12:03:59 -0300

    Add files via upload

---
+++ Avaliação victor matheus.sql
@@ -0,0 +1,13 @@
+use avaliação;
+
+SELECT COUNT(country_name) AS total
+FROM country;
+
+SELECT *
+FROM ranking_system;
+
+SELECT COUNT(DISTINCT university_name) AS total_universidades
+FROM university;
+
+SELECT AVG(student_staff_ratio) AS media_student_staff_ratio
+FROM university_year;
